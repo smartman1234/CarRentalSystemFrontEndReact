7bb6f5de7a08aaac7788fe6b81fe1378eba512e0
smartman1234 <john.smith.dev0724@gmail.com>
2022-07-23 18:24:52 +0530

    addedImage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,6 +11,12 @@ import {withStyles} from "@mui/styles";
 import benze from "../../assets/img/benze.jpg"
 import prius from "../../assets/img/prius.jpg"
 import Toyota from "../../assets/img/Toyota.jpg"
+import BMWM5 from "../../assets/img/BMWM5.jpg"
+import BMWMz4 from "../../assets/img/BMWz4.jpg"
+import ToyotaCorollaAltis from "../../assets/img/ToyotaCorollaAltis.jpg"
+import mitsubishi from "../../assets/img/mitsubishi.jpg"
+import mitsubishiEvo from "../../assets/img/mitsubishiEvo.jpg"
+import mitsubishiLanzerRed from "../../assets/img/mitsubishiLanzerRed.jpg"
 
 const styleSheet = () => ({
     container: {
@@ -77,7 +83,7 @@ class Home extends Component {
                                 </div>
                                 <CardContent>
                                     <Typography gutterBottom variant="h5" component="div">
-                                        Lizard
+                                        Benze
                                     </Typography>
                                     <Typography variant="body2" color="text.secondary">
                                         Lizards are a widespread group of squamate reptiles, with over 6,000
@@ -105,7 +111,7 @@ class Home extends Component {
                                 </div>
                                 <CardContent>
                                     <Typography gutterBottom variant="h5" component="div">
-                                        Lizard
+                                        Toyota
                                     </Typography>
                                     <Typography variant="body2" color="text.secondary">
                                         Lizards are a widespread group of squamate reptiles, with over 6,000
@@ -124,6 +130,148 @@ class Home extends Component {
                         </Card>
 
                     </Grid>
+
+                    <Grid item lg={3} md={3} sm={3} xm={3}>
+                        <Card sx={{maxWidth: 345}}>
+                            <CardActionArea className={classes.container}>
+                                <div className={classes.img__container}>
+                                    <img src={BMWM5} alt=""/>
+                                </div>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        BMWM5
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                                        species, ranging across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Link to={"/user"}>
+                                    <Button size="small" color="primary">
+                                        Booking
+                                    </Button>
+                                </Link>
+
+                            </CardActions>
+                        </Card>
+
+                    </Grid>
+
+
+                    <Grid item lg={3} md={3} sm={3} xm={3}>
+                        <Card sx={{maxWidth: 345}}>
+                            <CardActionArea className={classes.container}>
+                                <div className={classes.img__container}>
+                                    <img src={BMWMz4} alt=""/>
+                                </div>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        BMWMz4
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                                        species, ranging across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Link to={"/user"}>
+                                    <Button size="small" color="primary">
+                                        Booking
+                                    </Button>
+                                </Link>
+
+                            </CardActions>
+                        </Card>
+
+                    </Grid>
+
+
+                    <Grid item lg={3} md={3} sm={3} xm={3}>
+                        <Card sx={{maxWidth: 345}}>
+                            <CardActionArea className={classes.container}>
+                                <div className={classes.img__container}>
+                                    <img src={ToyotaCorollaAltis} alt=""/>
+                                </div>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        ToyotaCorollaAltis
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                                        species, ranging across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Link to={"/user"}>
+                                    <Button size="small" color="primary">
+                                        Booking
+                                    </Button>
+                                </Link>
+
+                            </CardActions>
+                        </Card>
+
+                    </Grid>
+                    <Grid item lg={3} md={3} sm={3} xm={3}>
+                        <Card sx={{maxWidth: 345}}>
+                            <CardActionArea className={classes.container}>
+                                <div className={classes.img__container}>
+                                    <img src={BMWMz4} alt=""/>
+                                </div>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        BMWMz4
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                                        species, ranging across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Link to={"/user"}>
+                                    <Button size="small" color="primary">
+                                        Booking
+                                    </Button>
+                                </Link>
+
+                            </CardActions>
+                        </Card>
+
+                    </Grid>
+
+                    <Grid item lg={3} md={3} sm={3} xm={3}>
+                        <Card sx={{maxHeight:350}}>
+                            <CardActionArea className={classes.container}>
+                                <div className={classes.img__container}>
+                                    <img src={mitsubishiEvo} alt=""/>
+                                </div>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        mitsubishiEvo
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                                        species, ranging across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Link to={"/user"}>
+                                    <Button size="small" color="primary">
+                                        Booking
+                                    </Button>
+                                </Link>
+
+                            </CardActions>
+                        </Card>
+
+                    </Grid>
+
 
                 </Grid>
 
